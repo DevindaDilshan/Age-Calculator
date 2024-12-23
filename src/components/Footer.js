18aa98ff3361d821641e12a8f8649aedd65a97f3
Devinda Dilshan <dilshandevinda88@gmail.com>
2024-12-23 21:03:12 +0530

    Refactor code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ function Footer() {
     return (
 
         <div className="p-5 bg-dark justify-content-center" data-bs-theme="dark">
-            <div className=" container p-2 text-white text-center">
+            <div className=" container p-3 text-white text-center">
                 <div className="row row-cols-3 fs-6">
                     <div className="col"></div>
                     <div className="col"></div>
